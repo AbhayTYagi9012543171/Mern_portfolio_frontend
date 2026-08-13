f50f6316cde56f9eeadc33e138259da1521f8fd3
Abhay Tyagi <tyagiabhay2004@gmail.com>
2026-08-13 18:52:34 +0530

    Add MERN portfolio frontend

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+
+export interface Project {
+  _id?: string;
+  slug: string;
+
+  title: string;
+  description: string;
+
+  image: string;
+  category: string;
+
+  technologies: string[];
+  features: string[];
+
+  github?: string;
+  liveDemo?: string;
+
+  featured: boolean;
+
+  createdAt?: string;
+  updatedAt?: string;
+}
